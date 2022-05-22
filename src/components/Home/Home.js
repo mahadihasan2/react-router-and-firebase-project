f9d18a576b51ee26ea8bffbfd50a528700d28b95
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-23 01:34:26 +0600

    use react-firebase-hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const { user } = useFirebase()
     return (
         <div>
             <h1>This is Home</h1>
+            <p>current User: {user ? user.displayName : 'No body'}</p>
         </div>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,19 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+// import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
+
+const auth = getAuth(app)
 const Login = () => {
-    const { signInWithGoogle } = useFirebase();
+    // const { signInWithGoogle } = useFirebase();
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth)
     return (
 
         <div>
             <h3>Please LogIn</h3>
-            <button onClick={signInWithGoogle}> signInWithGoogle </button>
+            {/* <button onClick={signInWithGoogle}> signInWithGoogle </button> */}
+            <button onClick={() => signInWithGoogle()}>SignInWithGoogle</button>
             <form action="">
                 <input type="text" placeholder='Enter your Name' />
                 <br></br>
